27fdee9f293c220799069b9970e61deef954b279
Diyajain3 <diyajain3003@gmail.com>
2026-04-20 23:33:57 +0530

    Implement solution to find max distance between houses

---
+++ TwoFurthestHousesWithDifferentColors.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxDistance(int[] colors) {
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<colors.length;i++)
+        {
+            for(int j=0;j<colors.length;j++)
+            {
+                if(colors[i]!=colors[j])
+                {
+                     max=Math.max(max,Math.abs(j-i));
+                }
+            }
+        }
+        return max==Integer.MAX_VALUE?0:max;
+    }
+}
